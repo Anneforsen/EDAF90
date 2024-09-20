4ab91f511d64a8eb5e06f56c73e2f2527c8f58ee
Anneforsen <jacoba.annefors@gmail.com>
2024-09-20 17:17:54 +0200

    lab0

---
+++ labs/lab0/src/lab0.js
@@ -0,0 +1,7 @@
+console.log("Hello World!");
+
+const elem = document.getElementById("my-div");
+elem.innerHTML = "<h1>Hello DOM</h1>";
+
+const formInput = '<img src="x" onerror="alert(\'your page has been hacked\');">';
+elem.innerHTML = formInput;
